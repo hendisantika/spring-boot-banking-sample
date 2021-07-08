ca138fd174ba0110d7493e26d363b57c81694951
Hendi Santika <hendisantika@gmail.com>
2021-07-08 19:19:12 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootbankingsample/SpringBootBankingSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootbankingsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBankingSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBankingSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootbankingsample/SpringBootBankingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootbankingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBankingSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
