7b679f9780eedd5a6afeca4080e994701e393f3c
Hendi Santika <hendisantika@gmail.com>
2021-07-14 10:52:38 +0700

    #12 - Add TransactionInput

---
+++ src/main/java/com/hendisantika/springbootbankingsample/util/TransactionInput.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootbankingsample.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-banking-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/07/21
+ * Time: 10.50
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionInput {
+    private AccountInput sourceAccount;
+
+    private AccountInput targetAccount;
+
+    @Positive(message = "Transfer amount must be positive")
+    // Prevent fraudulent transfers attempting to abuse currency conversion errors
+    @Min(value = 1, message = "Amount must be larger than 1")
+    private double amount;
+
+    private String reference;
+
+    @Min(value = -90, message = "Latitude must be between -90 and 90")
+    @Max(value = 90, message = "Latitude must be between -90 and 90")
+    private Double latitude;
+
+    @Min(value = -180, message = "Longitude must be between -180 and 180")
+    @Max(value = 180, message = "Longitude must be between -180 and 180")
+    private Double longitude;
+}
