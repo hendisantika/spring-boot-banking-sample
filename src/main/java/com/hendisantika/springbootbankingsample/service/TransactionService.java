a8d22230edb691280ad0ffc4f60306825d3f68a0
Hendi Santika <hendisantika@gmail.com>
2021-07-21 05:53:57 +0700

    #18 - Add makeTransfer in TransactionService

--- src/main/java/com/hendisantika/springbootbankingsample/service/TransactionService.java
+++ src/main/java/com/hendisantika/springbootbankingsample/service/TransactionService.java
@@ -1,9 +1,15 @@
 package com.hendisantika.springbootbankingsample.service;
 
+import com.hendisantika.springbootbankingsample.model.Account;
+import com.hendisantika.springbootbankingsample.model.Transaction;
 import com.hendisantika.springbootbankingsample.repository.AccountRepository;
 import com.hendisantika.springbootbankingsample.repository.TransactionRepository;
+import com.hendisantika.springbootbankingsample.util.TransactionInput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,39 @@ public class TransactionService {
 
     @Autowired
     private TransactionRepository transactionRepository;
+
+    public boolean makeTransfer(TransactionInput transactionInput) {
+        // TODO refactor synchronous implementation with messaging queue
+        String sourceSortCode = transactionInput.getSourceAccount().getSortCode();
+        String sourceAccountNumber = transactionInput.getSourceAccount().getAccountNumber();
+        Optional<Account> sourceAccount = accountRepository
+                .findBySortCodeAndAccountNumber(sourceSortCode, sourceAccountNumber);
+
+        String targetSortCode = transactionInput.getTargetAccount().getSortCode();
+        String targetAccountNumber = transactionInput.getTargetAccount().getAccountNumber();
+        Optional<Account> targetAccount = accountRepository
+                .findBySortCodeAndAccountNumber(targetSortCode, targetAccountNumber);
+
+        if (sourceAccount.isPresent() && targetAccount.isPresent()) {
+            if (isAmountAvailable(transactionInput.getAmount(), sourceAccount.get().getCurrentBalance())) {
+                var transaction = new Transaction();
+
+                transaction.setAmount(transactionInput.getAmount());
+                transaction.setSourceAccountId(sourceAccount.get().getId());
+                transaction.setTargetAccountId(targetAccount.get().getId());
+                transaction.setTargetOwnerName(targetAccount.get().getOwnerName());
+                transaction.setInitiationDate(LocalDateTime.now());
+                transaction.setCompletionDate(LocalDateTime.now());
+                transaction.setReference(transactionInput.getReference());
+                transaction.setLatitude(transactionInput.getLatitude());
+                transaction.setLongitude(transactionInput.getLongitude());
+
+                updateAccountBalance(sourceAccount.get(), transactionInput.getAmount());
+                transactionRepository.save(transaction);
+
+                return true;
+            }
+        }
+        return false;
+    }
 }
