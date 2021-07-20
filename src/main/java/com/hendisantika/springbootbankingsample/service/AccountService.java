11cbbf52ce65dea415abdbd07b56c9eee07340cf
Hendi Santika <hendisantika@gmail.com>
2021-07-21 05:53:02 +0700

    #16 - Add getAccount in AccountService

--- src/main/java/com/hendisantika/springbootbankingsample/service/AccountService.java
+++ src/main/java/com/hendisantika/springbootbankingsample/service/AccountService.java
@@ -1,9 +1,12 @@
 package com.hendisantika.springbootbankingsample.service;
 
+import com.hendisantika.springbootbankingsample.model.Account;
 import com.hendisantika.springbootbankingsample.repository.AccountRepository;
 import com.hendisantika.springbootbankingsample.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +25,15 @@ public class AccountService {
 
     @Autowired
     private TransactionRepository transactionRepository;
+
+    public Account getAccount(String sortCode, String accountNumber) {
+        Optional<Account> account = accountRepository
+                .findBySortCodeAndAccountNumber(sortCode, accountNumber);
+
+        account.ifPresent(value ->
+                value.setTransactions(transactionRepository
+                        .findBySourceAccountIdOrderByInitiationDate(value.getId())));
+
+        return account.orElse(null);
+    }
 }
