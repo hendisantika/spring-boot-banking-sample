19dbb232241da574107310b56f404fb33ca83858
Hendi Santika <hendisantika@gmail.com>
2021-07-23 16:02:53 +0700

    #25 - Add makeTransfer in TransactionRestController

--- src/main/java/com/hendisantika/springbootbankingsample/controller/TransactionRestController.java
+++ src/main/java/com/hendisantika/springbootbankingsample/controller/TransactionRestController.java
@@ -1,10 +1,20 @@
 package com.hendisantika.springbootbankingsample.controller;
 
 import com.hendisantika.springbootbankingsample.service.TransactionService;
+import com.hendisantika.springbootbankingsample.util.InputValidator;
+import com.hendisantika.springbootbankingsample.util.TransactionInput;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,6 +39,20 @@ public class TransactionRestController {
     @Autowired
     public TransactionRestController(TransactionService transactionService) {
         this.transactionService = transactionService;
+    }
+
+    @PostMapping(value = "/transactions",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> makeTransfer(
+            @Valid @RequestBody TransactionInput transactionInput) {
+        if (InputValidator.isSearchTransactionValid(transactionInput)) {
+//            new Thread(() -> transactionService.makeTransfer(transactionInput));
+            boolean isComplete = transactionService.makeTransfer(transactionInput);
+            return new ResponseEntity<>(isComplete, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(INVALID_TRANSACTION, HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
